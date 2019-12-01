13439ad6a5a5f2d1e04c361cffeb32929005b8a9
zlmgit <13477407066@163.com>
2019-12-01 18:43:02 +0800

    update

---
+++ hello-spring-cloud-alibaba-nacos-provider3/src/main/java/com/zlm/hello/spring/cloud/alibaba/nacos/provider3/config/MongoSettingsProperties.java
@@ -0,0 +1,36 @@
+package com.zlm.hello.spring.cloud.alibaba.nacos.provider3.config;
+
+import lombok.Data;
+import org.springframework.stereotype.Component;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@Data
+public class MongoSettingsProperties {
+    @NotBlank
+    private String database = "newDB";
+    @NotEmpty
+    private List<String> address = Arrays.asList("192.168.2.101:27017");
+    private Integer minConnectionsPerHost = 0;
+    private Integer maxConnectionsPerHost = 100;
+    private Integer threadsAllowedToBlockForConnectionMultiplier = 5;
+    private Integer serverSelectionTimeout = 30000;
+    private Integer maxWaitTime = 120000;
+    private Integer maxConnectionIdleTime = 0;
+    private Integer maxConnectionLifeTime = 0;
+    private Integer connectTimeout = 10000;
+    private Integer socketTimeout = 0;
+    private Boolean socketKeepAlive = false;
+    private Boolean sslEnabled = false;
+    private Boolean sslInvalidHostNameAllowed = false;
+    private Boolean alwaysUseMBeans = false;
+    private Integer heartbeatFrequency = 10000;
+    private Integer minHeartbeatFrequency = 500;
+    private Integer heartbeatConnectTimeout = 20000;
+    private Integer heartbeatSocketTimeout = 20000;
+    private Integer localThreshold = 15;
+}
---
+++ hello-spring-cloud-alibaba-nacos-provider3/src/main/java/com/zlm/hello/spring/cloud/alibaba/nacos/provider3/controller/MongoDbController.java
@@ -0,0 +1,64 @@
+package com.zlm.hello.spring.cloud.alibaba.nacos.provider3.controller;
+
+import java.util.List;
+
+import com.zlm.hello.spring.cloud.alibaba.nacos.provider3.model.Book;
+import com.zlm.hello.spring.cloud.alibaba.nacos.provider3.service.MongoDbService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class MongoDbController {
+
+    @Autowired
+    private MongoDbService mongoDbService;
+
+    @PostMapping("/mongo/save")
+    public String saveObj() {
+        Book book = new Book();
+        book.setInfo("小说");
+        return mongoDbService.saveObj(book);
+    }
+
+    @GetMapping("/mongo/findAll")
+    public List<Book> findAll() {
+        return mongoDbService.findAll();
+    }
+
+    @GetMapping("/mongo/findOne")
+    public Book findOne(@RequestParam String id) {
+        return mongoDbService.getBookById(id);
+    }
+
+    @GetMapping("/mongo/findOneByName")
+    public Book findOneByName(@RequestParam String name) {
+        return mongoDbService.getBookByName(name);
+    }
+
+    @PostMapping("/mongo/update")
+    public String update(@RequestBody Book book) {
+        return mongoDbService.updateBook(book);
+    }
+
+    @PostMapping("/mongo/delOne")
+    public String delOne(@RequestBody Book book) {
+        return mongoDbService.deleteBook(book);
+    }
+
+    @GetMapping("/mongo/delById")
+    public String delById(@RequestParam String id) {
+        return mongoDbService.deleteBookById(id);
+    }
+
+    @GetMapping("/mongo/findlikes")
+    public List<Book> findByLikes(@RequestParam String search) {
+        return mongoDbService.findByLikes(search);
+    }
+}
+
+
